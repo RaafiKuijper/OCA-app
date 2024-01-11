4e4c55c7907ecfd228da78cf9710b14b2430d536
RaafiKuijper <raafi.kuijper@itvitaelearning.nl>
2024-01-11 13:21:06 +0100

    Add getById/createOption endpoint for Option

--- backend/src/main/java/nl/itvitae/ocaapp/option/OptionService.java
+++ backend/src/main/java/nl/itvitae/ocaapp/option/OptionService.java
@@ -1,6 +1,8 @@
 package nl.itvitae.ocaapp.option;
 
+import java.util.Optional;
 import lombok.AllArgsConstructor;
+import nl.itvitae.ocaapp.question.Question;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,4 +15,11 @@ public class OptionService {
     return optionRepository.findAll();
   }
 
+  public Optional<Option> getById(Long id) {
+    return optionRepository.findById(id);
+  }
+
+  public Option createOption(Option option) {
+    return optionRepository.save(option);
+  }
 }
