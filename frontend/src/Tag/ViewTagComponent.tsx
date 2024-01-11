bd7cc6cc3e7b1f5b8034ddcf74a1907088783d19
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-01-11 10:00:52 +0100

    Create Tag Feature: Review Peter

--- frontend/src/Tag/ViewTagComponent.tsx
+++ frontend/src/Tag/ViewTagComponent.tsx
@@ -1,19 +1,27 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Tag from "./TagInterface";
 
 function ViewTagComponent() {
-  const [results, setResults] = useState([]);
+  const [tags, setTags] = useState<Tag[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await axios.get(`http://localhost:8080/api/v1/tags`);
-      setResults(res.data);
+      const response = await axios.get(`http://localhost:8080/api/v1/tags`);
+      setTags(response.data);
     };
 
     fetchData();
-  });
+  }, []);
 
-  return <>Tags in database: {results.map(tag => <p>{tag.name}</p>)}</>;
+  return (
+    <>
+      Tags in database:{" "}
+      {tags.map((tag) => (
+        <p key={tag.id}>{tag.name}</p>
+      ))}
+    </>
+  );
 }
 
 export default ViewTagComponent;
