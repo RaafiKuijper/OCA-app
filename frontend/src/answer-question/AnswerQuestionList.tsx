f22d5e0483cb5e39eaf8434c72db62384eb5cb52
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-01-12 10:35:24 +0100

    Answer Question List Styling

--- frontend/src/answer-question/AnswerQuestionList.tsx
+++ frontend/src/answer-question/AnswerQuestionList.tsx
@@ -2,11 +2,12 @@ import { InputGroup } from "react-bootstrap";
 import { Option } from "../question/questionModels";
 import classes from "../styles/answer-question.module.css";
 
+// This does not work for long lines of text yet.
 const AnswerQuestionList = (props: { options: Option[] }) => {
   return (
     <section className={classes.answerQuestionList}>
       {props.options.map((option) => (
-        <InputGroup key={option.id} className={classes.answerQuestionListGroup}>
+        <InputGroup key={option.id} className={classes.answerQuestionListItem}>
           <InputGroup.Checkbox className={classes.answerQuestionListCheckbox} />
           <InputGroup.Text className={classes.answerQuestionListText}>
             {option.text}
