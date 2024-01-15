9607bc737bcb5afe41ee046e1d39fd84dd08ff26
RaafiKuijper <raafi.kuijper@itvitaelearning.nl>
2024-01-15 14:47:32 +0100

    Add Route for createquestionforncomponent

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AnswerQuestionView from "./answer-question/AnswerQuestionView.tsx";
+import CreateQuestionsFormComponent from "./question/CreateQuestionFormComponent.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/answer-question/:id" element={<AnswerQuestionView />} />
+        <Route path="/create-question" element={<CreateQuestionsFormComponent />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
