a3965e4157d1317c3e89b74c16f04e05469f0028
RaafiKuijper <raafi.kuijper@itvitaelearning.nl>
2024-01-15 15:09:21 +0100

    Prevent creating questions with 1 option, all or no correct options

--- frontend/src/question/CreateQuestionComponent.tsx
+++ frontend/src/question/CreateQuestionComponent.tsx
@@ -8,30 +8,41 @@ function CreateQuestionsComponent(props: QuestionProps) {
 
   useEffect(() => {
     const fetchData = async () => {
-      let currentOptionsIsCorrect = props.optionsIsCorrect.map((isCorrect) =>
-        isCorrect === undefined ? false : isCorrect
-      );
+      if (
+        props.optionsIsCorrect.filter((isCorrect) => isCorrect === false)
+          .length === props.optionsIsCorrect.length ||
+        props.optionsIsCorrect.filter((isCorrect) => isCorrect === true)
+          .length === props.optionsIsCorrect.length
+      ) {
+        setResult("invalid amount of correct answers");
+      } else if (props.optionCount <= 1) {
+        setResult("invalid amount of options");
+      } else {
+        props.optionsIsCorrect.map((isCorrect) =>
+          isCorrect === undefined ? false : isCorrect
+        );
 
-      let options: OptionBody[] = [];
+        let options: OptionBody[] = [];
 
-      for (let i = 0; i < props.optionCount; i++) {
-        options[i] = {
-          text: props.optionsText[i],
-          isCorrect: props.optionsIsCorrect[i],
-        };
-      }
-
-      const createQuestion = await axios.post(
-        "http://localhost:8080/api/v1/questions/create",
-        {
-          text: props.text,
-          options: options,
-          explanation: props.explanation,
+        for (let i = 0; i < props.optionCount; i++) {
+          options[i] = {
+            text: props.optionsText[i],
+            isCorrect: props.optionsIsCorrect[i],
+          };
         }
-      );
 
-      if (createQuestion.status == 201) setResult("ok");
-      else setResult("failed");
+        const createQuestion = await axios.post(
+          "http://localhost:8080/api/v1/questions/create",
+          {
+            text: props.text,
+            options: options,
+            explanation: props.explanation,
+          }
+        );
+
+        if (createQuestion.status == 201) setResult("ok");
+        else setResult("failed");
+      }
     };
     fetchData();
   }, []);
