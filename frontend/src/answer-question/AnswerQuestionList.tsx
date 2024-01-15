894a7eae3430405682ce40966d74dbb55086ecc4
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-01-15 10:59:56 +0100

    Clickable Text Answer Question List

--- frontend/src/answer-question/AnswerQuestionList.tsx
+++ frontend/src/answer-question/AnswerQuestionList.tsx
@@ -1,6 +1,7 @@
 import { InputGroup } from "react-bootstrap";
 import { Option } from "../question/questionModels";
 import classes from "../styles/answer-question.module.css";
+import { useState } from "react";
 
 // This does not work for long lines of text yet.
 const AnswerQuestionList = (props: {
@@ -8,6 +9,7 @@ const AnswerQuestionList = (props: {
   selectedOptions: number[];
   setSelectedOptions: (options: number[]) => void;
 }) => {
+  const [checked, setChecked] = useState<boolean[]>([]);
   const updateSelectedOptions = (selectedId: number) => {
     let updatedOptions = [];
     if (props.selectedOptions.includes(selectedId)) {
@@ -16,22 +18,38 @@ const AnswerQuestionList = (props: {
       updatedOptions = [...props.selectedOptions, selectedId];
     }
     props.setSelectedOptions(updatedOptions);
+    updateChecked(selectedId);
   };
+
+  const updateChecked = (id: number) => {
+    const newChecked = checked;
+    newChecked[id] = !checked[id];
+    setChecked(newChecked);
+  };
+
   return (
     <section className={classes.answerQuestionList}>
       {props.options.map((option) => (
         <InputGroup key={option.id} className={classes.answerQuestionListItem}>
           <InputGroup.Checkbox
-            onChange={() => updateSelectedOptions(option.id)}
+            onChange={() => {
+              updateSelectedOptions(option.id);
+            }}
             className={classes.answerQuestionListCheckbox}
             style={{
               margin: "0",
               height: "1.5em",
               width: "1.5em",
             }}
+            checked={
+              checked[option.id] === undefined ? false : checked[option.id]
+            }
           />
           <InputGroup.Text
             className={classes.answerQuestionListText}
+            onClick={() => {
+              updateSelectedOptions(option.id);
+            }}
             style={{
               padding: "0.8em",
               fontSize: "1.2em",
