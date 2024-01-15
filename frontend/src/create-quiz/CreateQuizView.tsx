867e493729bb214d014930238f92e46b7fcc726e
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-01-15 14:16:26 +0100

    View Question Count

--- frontend/src/create-quiz/CreateQuizView.tsx
+++ frontend/src/create-quiz/CreateQuizView.tsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import Header from "../styled-components/header/Header";
 import CreateQuizQuestionCount from "./CreateQuizQuestionCount";
+import axios from "axios";
+import QuestionCount from "./QuestionCount";
 
 const CreateQuizView = () => {
+  const [questionCount, setQuestionCount] = useState(0);
+
+  useEffect(() => {
+    const getQuestionCount = async () => {
+      const result = await axios.get(
+        "http://localhost:8080/api/v1/questions/count"
+      );
+      const data: QuestionCount = result.data;
+      setQuestionCount(data.count);
+    };
+    getQuestionCount();
+  });
+
   return (
     <>
       <Header text="Create Quiz View" />;
-      <CreateQuizQuestionCount />
+      <CreateQuizQuestionCount questionCount={questionCount} />
     </>
   );
 };
