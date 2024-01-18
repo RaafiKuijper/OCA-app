c95e5c91a20133fa9b584f65f80267a16f2ff25a
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-01-18 13:14:47 +0100

    Home + Navbar: Add Navbar to all pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,11 @@
-import { Navbar } from "react-bootstrap";
 import "./styles/App.css";
 import isAdmin from "./user/user";
-import AdminNavLinks from "./navbar/admin-nav-links/AdminNavLinks";
-import UserNavbar from "./navbar/user-navbar/UserNavLinks";
 import AdminHome from "./home/admin-home/AdminHome";
 import UserHome from "./home/user-home/UserHome";
 
 // render current route component
 function App() {
-  return (
-    <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        {isAdmin ? <AdminNavLinks /> : <UserNavbar />}
-      </Navbar>
-      {isAdmin ? <AdminHome /> : <UserHome />}
-    </>
-  );
+  return <>{isAdmin ? <AdminHome /> : <UserHome />}</>;
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,9 +8,15 @@ import MakeQuizView from "./make-quiz/MakeQuizView.tsx";
 import CreateQuestionsFormComponent from "./question/CreateQuestionFormComponent.tsx";
 import CreateTagComponent from "./tag/CreateTagComponent.tsx";
 import isAdmin from "./user/user.tsx";
+import { Navbar } from "react-bootstrap";
+import AdminNavLinks from "./navbar/admin-nav-links/AdminNavLinks.tsx";
+import UserNavLinks from "./navbar/user-nav-links/UserNavLinks.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Navbar bg="dark" data-bs-theme="dark">
+      {isAdmin ? <AdminNavLinks /> : <UserNavLinks />}
+    </Navbar>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
