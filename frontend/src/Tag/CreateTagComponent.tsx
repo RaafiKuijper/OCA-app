988470a54bda3ee9884d0a1f21efef64bb6c2afa
NolKeereweer <nol.keereweer202@gmail.com>
2024-01-22 13:44:04 +0100

    Functional frontend feature
    Sadly couldn't make the feature work in a separate file. Had to put it into CreateTagComponent. Through count it updates ViewTag as well.

--- frontend/src/Tag/CreateTagComponent.tsx
+++ frontend/src/Tag/CreateTagComponent.tsx
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 import ViewTagComponent from "./ViewTagComponent";
 import Header from "../headers/header/Header";
 import classes from "../styles/Create-Tag.module.css";
-import DeleteTagComponent from "./DeleteTagComponent";
 
 function CreateTagComponent() {
+  const [tag, setTag] = useState<string>("");
   const [tagName, setTagName] = useState<string>("");
   const [tagChapter, setTagChapter] = useState<string>("");
   const [tagSummary, setTagSummary] = useState<string>("");
@@ -34,6 +34,19 @@ function CreateTagComponent() {
         console.error("Can't submit pre-existing Tag.");
         setDispText(`That tag already exists`);
       }
+    }
+  };
+
+  const deleteData = async () => {
+    const checker = await axios.get(
+      `http://localhost:8080/api/v1/tags/findByName/${tag}`
+    );
+    if (checker.data[0] == null) {
+      setDispText(`${tag} doesn't match any names in the database.`);
+    } else {
+      await axios.delete(`http://localhost:8080/api/v1/tags/deleteTag/${tag}`);
+      setDispText(``);
+      setCount(count + 1);
     }
   };
 
@@ -89,8 +102,28 @@ function CreateTagComponent() {
         <p>{dispText}</p>
       </form>
 
-      <DeleteTagComponent />
       <ViewTagComponent count={count} />
+
+      <form
+        className={classes.createTagView}
+        onSubmit={(e) => {
+          e.preventDefault();
+          deleteData();
+        }}
+      >
+        Delete a tag by submitting it's name:
+        <br />
+        <input
+          className={classes.deleteTagInput}
+          type="text"
+          placeholder="Tag name"
+          onChange={(e) => setTag(e.target.value)}
+        ></input>
+        <br />
+        <button className={classes.tagSubmitButton} type="submit">
+          Submit
+        </button>
+      </form>
     </>
   );
 }
